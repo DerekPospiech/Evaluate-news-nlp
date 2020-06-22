9e6946510860bbd3170a7a6f160306105cc5a839
Derek Pospiech <45837749+DerekPospiech@users.noreply.github.com>
2020-06-21 21:12:46 -0400

    updated results section to include additional info

--- src/client/js/newsChecker.js
+++ src/client/js/newsChecker.js
@@ -26,7 +26,9 @@ function checkForNews(inputText) {
       postData('/', inputText)
       .then(response=>{
         console.log("this is the data trying to be outputted into the response section" + response)
-        document.getElementById('results').innerHTML = response.polarity
+        document.getElementById('polarity').innerHTML = response.polarity
+        document.getElementById('text').innerHTML = response.text
+        document.getElementById('subjectivity').innerHTML = response.subjectivity
   // Dear student, you should create different UI elements to show different attributes of result such as polarity, text, subjectivity 
       })
     }
