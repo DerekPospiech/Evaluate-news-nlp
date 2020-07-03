df5dd4c4d7efab369b1db319c0ba2b5d2e8e2486
Derek Pospiech <45837749+DerekPospiech@users.noreply.github.com>
2020-07-03 10:43:46 -0400

    fixed newschecker.test.  It works!!

--- src/client/js/newsChecker.js
+++ src/client/js/newsChecker.js
@@ -1,28 +1,6 @@
 function checkForNews(inputText) {
     console.log("::: Running checkForNews on ::", inputText)
 
-    // POST method:
-    const postData = async (url = '', data = {})=>{
-        // Default options are marked with *
-        const res = await fetch(url, {
-          method: 'POST', 
-          credentials: 'same-origin', 
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({text:data})  
-        });
-    
-        try {
-            const newData = await res.json();
-           // console.log(newData);
-            return newData;
-          }  catch(error) {
-            console.log("error", error);
-            // appropriately handle the error
-          }
-      }
-
       postData('/', inputText)
       .then(response=>{
         console.log("this is the data trying to be outputted into the response section " + response)
@@ -32,4 +10,27 @@ function checkForNews(inputText) {
       })
     }
 
+    // POST method:
+    const postData = async (url = '', data = {})=>{
+      // Default options are marked with *
+      const res = await fetch(url, {
+        method: 'POST', 
+        credentials: 'same-origin', 
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({text:data})  
+      });
+  
+      try {
+          const newData = await res.json();
+         // console.log(newData);
+          return newData;
+        }  catch(error) {
+          console.log("error", error);
+          // appropriately handle the error
+        }
+    }
+
 export { checkForNews }
+export {postData}
